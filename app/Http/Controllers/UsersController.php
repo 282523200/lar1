40c91121e9c3feb6a67d8944c6f051df508b9fc3
xusl <xusl_1979@hotmail.com>
2020-05-12 12:46:14 +0800

    a

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+    //用户显示
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    //注册账号
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
--- app/User.php
+++ app/User.php
@@ -22,4 +22,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    //定义 gravatar
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
